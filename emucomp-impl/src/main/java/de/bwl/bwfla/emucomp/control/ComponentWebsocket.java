c50a16409b086b9ca227faa94b215b3cab51eccc
peeqle <lobach.artem.v@gmail.com>
2025-02-21 15:42:59 +0300

    quarkus, modules & soap

--- emucomp-impl/src/main/java/de/bwl/bwfla/emucomp/control/ComponentWebsocket.java
+++ emucomp-impl/src/main/java/de/bwl/bwfla/emucomp/control/ComponentWebsocket.java
@@ -12,7 +12,6 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import javax.ws.rs.PathParam;
-
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
@@ -23,45 +22,58 @@ import java.util.logging.Logger;
 public class ComponentWebsocket {
     @Inject
     protected NodeManager nodeManager;
-    
+
     protected Session session;
     protected String componentId;
-    
+
     @OnOpen
     public void open(@PathParam("componentId") String componentId, Session session) {
         this.session = session;
         this.componentId = componentId;
     }
-    
+
     @OnMessage
     public void recv(Session session, String data) {
         JsonObject message = Json.createReader(new StringReader(data)).readObject();
-        
+
         String request = message.getString("request");
         if (request == null) {
             return;
         }
-        
-        switch (request) {
-        case "getState":
-            String state = nodeManager.getComponentById(componentId, EmulatorComponent.class).getEmulatorState();
-            JsonObject response = Json.createObjectBuilder()
-                    .add("state", state)
-            .build();
-            session.getAsyncRemote().sendText(response.toString());
 
-            break;
-        case "echo":
-            session.getAsyncRemote().sendText(message.getString("message"));
-            break;
-        case "keepalive":
-            nodeManager.keepalive(componentId);
-            break;
-        default:
-            break;                
+        switch (request) {
+            case "getState":
+                try {
+                    String state = nodeManager.getComponentById(componentId, EmulatorComponent.class).getEmulatorState();
+                    JsonObject response = Json.createObjectBuilder()
+                            .add("state", state)
+                            .build();
+                    session.getAsyncRemote().sendText(response.toString());
+                } catch (BWFLAException e) {
+                    JsonObject response = Json.createObjectBuilder()
+                            .add("error", "Could not get component state")
+                            .add("message", e.getMessage())
+                            .add("stacktrace", getStackTrace(e))
+                            .build();
+                    session.getAsyncRemote().sendText(response.toString());
+                }
+                break;
+            case "echo":
+                session.getAsyncRemote().sendText(message.getString("message"));
+                break;
+            case "keepalive":
+                try {
+                    nodeManager.keepalive(componentId);
+                } catch (BWFLAException e) {
+                    // TODO Auto-generated catch block
+                    Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+                }
+                break;
+            default:
+                break;
         }
     }
-    
+
     protected String getStackTrace(Throwable t) {
         StringWriter sw = new StringWriter();
         t.printStackTrace(new PrintWriter(sw));
